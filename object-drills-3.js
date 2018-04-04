054e5eab34cb5bcce40923d525c769d7796f3944
Craig Pounds <nweaver@windstream.net>
2018-04-04 14:44:40 -0500

    added object dirlls 6

--- object-drills-3.js
+++ object-drills-3.js
@@ -64,6 +64,31 @@ const persons2 = [
 
 persons2.forEach(e => {
   let message = e.hasOwnProperty('boss') ? `${e.jobTitle} ${e.name} reports to ${e.boss}` : `${e.jobTitle} ${e.name} doesn't report to anybody`;
-  console.log(message);
+  //console.log(message);
 });
+
+
+const toBeDecoded = 'craft block argon meter bells brown croon droop';
+
+const cipher = {
+  a: 1,
+  b: 2,
+  c: 3,
+  d: 4,
+};
+
+function decodeWords(string) {
+  let words = string.split(' ');
+  let decodedWord = '';
+  words.forEach(e => decodedWord += decode(e));
+  return decodedWord;
+}
+
+function decode(string) {
+  if (cipher.hasOwnProperty(string[0])) {
+    return string.charAt(cipher[string[0]]);
+  }
+  return ' ';
+}
+//console.log(decodeWords(toBeDecoded));
 
